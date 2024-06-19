1e03e2f6ef8e0953d7a53e3574bb5544582d5719
Nainala Srimannarayana <105102064+sriman417@users.noreply.github.com>
2024-06-19 18:33:31 +0530

    Create arrays.ts

---
+++ arrays.ts
@@ -0,0 +1,108 @@
+// Object Types Challenge
+// Based on what we discussed we need to make up our Property Objects and array,
+// can you create that array, making sure to assign the correct Types?
+
+import { showReviewTotal, populateUser } from './utils'
+let isOpen: boolean
+
+// Reviews
+const reviews : { 
+    name: string; 
+    stars: number; 
+    loyaltyUser: boolean; 
+    date: string
+    }[] = [
+    {
+        name: 'Sheia',
+        stars: 5,
+        loyaltyUser: true,
+        date: '01-04-2021'
+    },
+    {
+        name: 'Andrzej',
+        stars: 3,
+        loyaltyUser: false,
+        date: '28-03-2021'
+    },
+    {
+        name: 'Omar',
+        stars: 4,
+        loyaltyUser: true,
+        date: '27-03-2021'
+    },
+]
+
+// User
+const you: {
+    firstName: string;
+    lastName: string;
+    isReturning: boolean;
+    age: number;
+    stayedAt: string[]
+} = {
+    firstName: 'Bobby',
+    lastName: 'Brown',
+    isReturning: true,
+    age: 35,
+    stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
+}
+
+//Properties
+const properties : {            //solution
+    image: string;
+    title: string;
+    price: number;
+    location: {
+        firstLine: string;
+        city: string;
+        code: number;
+        country: string;
+    };
+    contact: string;
+    isAvailable: boolean;
+}[] = [
+    {
+        image: '',
+        title: 'India flat',
+        price: 45,
+        location: {
+            firstLine: 'delhi 37',
+            city: 'Delhi',
+            code: 45632,
+            country: 'India'
+        },
+        contact: 'rentsforyou@gmail.com',
+        isAvailable: true  
+    },
+    {
+        image: '',
+        title: 'Polish Cottage',
+        price: 34,
+        location: {
+            firstLine: 'no 23',
+            city: 'Boston',
+            code: 343903,
+            country: 'USA'
+        },
+        contact: 'cotteges@hotmail.com',
+        isAvailable: false 
+    },
+    {
+        image: '',
+        title: 'Canada Flat',
+        price: 23,
+        location: {
+            firstLine: 'flat 15',
+            city: 'Canada',
+            code: 35433,
+            country: 'Canada',
+        },
+        contact: 'myrents@aol.com',
+        isAvailable: true
+    }
+]
+
+// Functions
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
+
+populateUser(you.isReturning, you.firstName)
